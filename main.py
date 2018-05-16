17b8726ab66b77d79a12bc0ecd238682d6a156ec
Arsh Chauhan <achauhan@alaska.edu>
2018-05-16 10:44:09 -0400

    Added scripts to restart an use uwsgi Handle multiple clients with non-equal auto-refresh rates for latest

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.5
 #
 # Project: Video Streaming with Flask
 # Author: Log0 <im [dot] ckieric [at] gmail [dot] com>
@@ -13,10 +13,11 @@
 # 1. Install Python dependencies: cv2, flask. (wish that pip install works like a charm)
 # 2. Run "python main.py".
 # 3. Navigate the browser to the local webpage.
-from flask import Flask, render_template, Response, send_file, make_response
+from flask import Flask, render_template, Response, send_file, make_response, request
 from camera import VideoCamera
 import subprocess
-import cv2
+from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -36,19 +37,27 @@ def gen(camera):
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    	 mimetype='multipart/x-mixed-replace; boundary=frame')
+	#return "Disabled for now"
+
 @app.route('/latest')
 def latest_image():
+	if not os.path.exists("captured"):
+		os.makedirs("captured")
 	return render_template('get_image.html')
 
 @app.route('/get_image')
 def get_image():
-	ffmpeg = subprocess.run("ffmpeg -i /dev/video1 -frames 1 latest.jpeg -y", shell=True)
+	image_dir = "captured/"
+	last_image_name_unix = float(request.args.get('time'))/1000
+	print(datetime.fromtimestamp(last_image_name_unix))
+	last_image_name = datetime.fromtimestamp(last_image_name_unix).strftime("%Y%m%d_%H%M%S")+".jpeg"
+	ffmpeg = subprocess.run("ffmpeg -i /dev/video1 -frames 1 -y "+ image_dir+last_image_name, shell=True)
 	# img = cv2.imread('latest.jpeg',0)
 	# frame = img.tobytes()
 	# return Response(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + 
 	# 	frame + b'\r\n\r\n',mimetype='multipart/x-mixed-replace; boundary=frame')
-	response = make_response(send_file('latest.jpeg', mimetype='image/jpeg'))
+	response = make_response(send_file(image_dir+last_image_name, mimetype='image/jpeg'))
 	response.headers['Cache-Control'] = "no-cache, no-store, must-revalidate"
 	response.headers["Pragma"] = "no-cache"
 	response.headers["Expires"] = "0"
@@ -56,4 +65,4 @@ def get_image():
 	
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True,threaded=True)
